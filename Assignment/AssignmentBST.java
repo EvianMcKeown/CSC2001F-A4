58de2443b760989f14b30819fbc6bf1135ab5d7b
EvianMcKeown <evian.mck@gmail.com>
2023-04-15 17:26:21 +0200

    -struggling with the defining the generics

---
+++ Assignment/AssignmentBST.java
@@ -0,0 +1,54 @@
+package Assignment;
+
+import java.util.Scanner;
+import Assignment.AVLTree;
+
+public class AssignmentBST {
+    public static void main(String args[]) {
+        dialog1();
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+
+        switch (s) {
+            case "1": // find the profile description for a given account
+
+                break;
+            case "2": // list all accounts
+                AVLTree<User> bs = new AVLTree<User>();
+                break;
+            case "3": // create an account
+
+                break;
+            case "4": // delete an account
+
+                break;
+            case "5": // display all posts for a single account
+
+                break;
+            case "6": // add a new post for a single account
+
+                break;
+            case "7": // load a file of actions from disk and process this
+
+                break;
+            case "8": // quit
+
+                break;
+            default:
+                break;
+        }
+
+        // remember to close scanner
+        in.close();
+    }
+
+    private static void dialog1() {
+        System.out.println("Choose an action from the menu" + System.lineSeparator()
+                + "1. Find the profile description for a given account" + System.lineSeparator()
+                + "2. List all accounts" + System.lineSeparator() + "3. Create an account" + System.lineSeparator()
+                + "4. Delete an account" + System.lineSeparator() + "5. Display all posts for a single account"
+                + System.lineSeparator() + "6. Add a new post for a single account" + System.lineSeparator()
+                + "7. Load a file of actions from disk and process this" + System.lineSeparator() + "8. Quit");
+        System.out.print("Enter your choice: ");
+    }
+}
---
+++ Assignment/User.java
@@ -0,0 +1,50 @@
+package Assignment;
+
+public class User <T extends Comparable<?>> {
+    private String name;
+    private String description;
+
+    public User(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (description == null) {
+            if (other.description != null)
+                return false;
+        } else if (!description.equals(other.description))
+            return false;
+        return true;
+    }
+
+}
